c7f67542e3a49074d2ec6194f8bf9aa6854841a2
Mishleyn <svalov1985@bk.ru>
2024-03-27 14:36:02 +0700

    добавил валидацию для крипты, кошелька и пула

---
+++ src/Management/MNX.MonitoringCenter.Management.UseCases/Commands/Pools/PoolModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace MNX.MonitoringCenter.Management.UseCases.Commands.Pools;
+
+public class PoolModelValidator : AbstractValidator<PoolInputModel>
+{
+    public PoolModelValidator()
+    {
+        RuleFor(x => x.Port)
+            .Must(port => 0 <= port && port <= 65535)
+            .WithMessage("The port must be in the range [0,65535]");
+
+        RuleFor(x => x.Domain.Length)
+            .NotEmpty()
+            .Must(domain => domain <= 40)
+            .WithMessage("The length of the domain should be in range of [1,40] characters.");
+    }
+}
---
+++ src/Management/MNX.MonitoringCenter.Management.UseCases/Commands/Wallets/WalletModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace MNX.MonitoringCenter.Management.UseCases.Commands.Wallets;
+
+/// <summary>
+/// Валидатор модели кошелька.
+/// </summary>
+public class WalletModelValidator : AbstractValidator<WalletInputModel>
+{
+    public WalletModelValidator()
+    {
+        RuleFor(x => x.Name.Length)
+            .NotEmpty()
+            .Must(name => name <= 30)
+            .WithMessage("The length of the wallet name should be in range of [1,30] characters.");
+
+        RuleFor(x => x.Address.Length)
+            .NotEmpty()
+            .Must(address => address <= 60)
+            .WithMessage("The length of the address should be in range of [1,60] characters.");
+    }
+}
