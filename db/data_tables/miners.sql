c5bcf4feb371f567e1078c387439a76771e13623
Daniil Ibragimov <ghaghal93@gmail.com>
2025-02-17 22:02:37 +0700

    Added new algos

--- db/data_tables/miners.sql
+++ db/data_tables/miners.sql
@@ -6,16 +6,16 @@ insert into monitoring_center.miners(name, version, mining_mode, supported_devic
 
 ('wildrig-multi', '0.42.2', 'Single', 1+2+8),
 
-('T-Rex', '0.26.8', 'Dual', 1),
+('t-rex', '0.26.8', 'Dual', 1),
 
-('TeamRedMiner', '0.10.21', 'Triple', 1),
+('teamredminer', '0.10.21', 'Triple', 1),
 
-('SRBMiner-Multi', '2.7.6', 'Triple', 1+2+4+8+16),
+('SRBMiner-MULTI', '2.7.6', 'Triple', 1+2+4+8+16),
 
-('Rigel', '1.20.1', 'Triple', 1),
+('rigel', '1.20.1', 'Triple', 1),
 
-('OneZeroMiner', '1.4.3', 'Dual', 1),
+('onezerominer', '1.4.3', 'Dual', 1),
 
-('Lolminer', '1.94a', 'Single', 1+2+8),
+('lolMiner', '1.94a', 'Dual', 1+2+8),
 
-('GMiner', '3.44', 'Single', 1+8);
+('gminer', '3.44', 'Single', 1+8);
