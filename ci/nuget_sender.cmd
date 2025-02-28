320d7dd20a0619b98f56bdfcdc97b42b3fcdd49c
Sahnov Alexandr <124876649+SahnovAlex@users.noreply.github.com>
2025-02-28 18:59:01 +0700

    features/feature_build_script: теперь версия пакетов извлекается из .csproj-файлов

--- ci/nuget_sender.cmd
+++ ci/nuget_sender.cmd
@@ -1,25 +1,37 @@
+@echo off
 SET host=77.37.200.24:5554
 
-cd ../
-cd ./src
+cd /d %~dp0\..
 
-cd ./Inventory
-cd ./MNX.MonitoringCenter.Inventory.Contracts/bin/Release
+call :PushPackage "src\Inventory\MNX.MonitoringCenter.Inventory.Contracts" "MNX.MonitoringCenter.Inventory.Contracts.csproj"
 
-dotnet nuget push -s http://%host%/v3/index.json -k NUGET-SERVER-API-KEY MNX.MonitoringCenter.Inventory.Contracts.1.0.5.nupkg
+call :PushPackage "src\Management\MNX.MonitoringCenter.Management.Agent.Commands" "MNX.MonitoringCenter.Management.Agent.Commands.csproj"
 
-cd ../../../../
-cd ./Management
-cd ./MNX.MonitoringCenter.Management.Agent.Commands/bin/Release
-
-dotnet nuget push -s http://%host%/v3/index.json -k NUGET-SERVER-API-KEY MNX.MonitoringCenter.Management.Agent.Commands.1.0.5.nupkg
-
-cd ../../../../
-cd ./Traffic
-cd ./MNX.MonitoringCenter.Traffic.Contracts.Bus/bin/Release
-
-dotnet nuget push -s http://%host%/v3/index.json -k NUGET-SERVER-API-KEY MNX.MonitoringCenter.Traffic.Contracts.Bus.1.0.3.nupkg
-
-cd ../../../../
+call :PushPackage "src\Traffic\MNX.MonitoringCenter.Traffic.Contracts.Bus" "MNX.MonitoringCenter.Traffic.Contracts.Bus.csproj"
 
 pause
+exit /b
+
+:: Функция для извлечения версии и публикации пакета
+:PushPackage
+setlocal
+set projectPath=%1
+set csprojFile=%2
+set version=
+
+for /f "delims=" %%i in ('powershell -Command "[xml]$csproj = Get-Content \"%projectPath%\\%csprojFile%\"; $csproj.Project.PropertyGroup.Version"') do set version=%%i
+
+set version=%version: =%
+
+echo Version extracted: %version%
+
+if exist %projectPath%\bin\Release (
+    cd %projectPath%\bin\Release
+) else (
+    echo Error: Folder %projectPath%\bin\Release does not exists!
+    pause
+)
+
+dotnet nuget push -s http://%host%/v3/index.json -k NUGET-SERVER-API-KEY %csprojFile:~0,-8%.%version%.nupkg
+endlocal
+goto :eof
