86af17e04e28ba02e0139f144a7d64532539cd78
Андрей Захаров <andryushka.zakharov.04@mail.ru>
2025-03-10 16:46:00 +0700

    Исправлены ошибки в модуле по обработке трафика

--- src/Traffic/MNX.MonitoringCenter.Traffic.Observers/Mapping/MiningIndicatorsBuilder.cs
+++ src/Traffic/MNX.MonitoringCenter.Traffic.Observers/Mapping/MiningIndicatorsBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using MNX.Application.UseCases.Results;
 using MNX.MonitoringCenter.Management.Contracts.FlightSheet;
 using MNX.MonitoringCenter.Management.Contracts.MiningDevice;
@@ -19,10 +20,15 @@ public class MiningIndicatorsBuilder
 
     private readonly IMediator _mediator;
 
-    public MiningIndicatorsBuilder(IMapper mapper, IMediator mediator)
+    private readonly ILogger<MiningIndicatorsBuilder> _logger;
+
+    public MiningIndicatorsBuilder(IMapper mapper,
+                                   IMediator mediator,
+                                   ILogger<MiningIndicatorsBuilder> logger)
     {
         _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
     /// <summary>
@@ -40,37 +46,54 @@ public class MiningIndicatorsBuilder
 
             foreach (var deviceIndicators in miningIndicators.Devices)
             {
-                var deviceGettingResult = await _mediator.Send(
+                try
+                {
+                    var deviceGettingResult = await _mediator.Send(
                     new GetDeviceByIdQuery(deviceIndicators.DeviceId, rigIndicators.UserId));
 
-                if (!TryGetValue(deviceGettingResult, out MiningDeviceModel device))
-                {
-                    continue;
+                    if (!TryGetValue(deviceGettingResult, out MiningDeviceModel device))
+                    {
+                        continue;
+                    }
+
+                    if (!device.FlightSheetId.HasValue || deviceIndicators.FlightSheet is null)
+                    {
+                        if (!device.FlightSheetId.HasValue && deviceIndicators.FlightSheet is null)
+                        {
+                            continue;
+                        }
+
+                        _logger.LogWarning(
+                            "Flight sheet presence is different: DB - {db}, Agent - {agent}",
+                            device.FlightSheetId,
+                            deviceIndicators.FlightSheet);
+
+                        continue;
+                    }
+
+                    var flightSheetGettingResult = await _mediator.Send(
+                            new GetFlightSheetByIdQuery(device.FlightSheetId.Value, rigIndicators.UserId));
+
+                    if (!TryGetValue(flightSheetGettingResult, out FlightSheetModel flightSheet))
+                    {
+                        continue;
+                    }
+
+                    // todo: перейти на сравнение по типу устройства.
+                    var flightSheetTarget = flightSheet.Targets.First(x => x.Miner.Name == deviceIndicators.FlightSheet.MinerName);
+
+                    deviceIndicators.FlightSheet.Id = flightSheet.Id;
+                    deviceIndicators.FlightSheet.MinerId = flightSheetTarget.Miner.Id;
+
+                    for (int i = 0; i < deviceIndicators.FlightSheet.Coins.Count; i++)
+                    {
+                        var coinMetrics = deviceIndicators.FlightSheet.Coins[i];
+                        coinMetrics.CoinId = flightSheetTarget.MiningConfig.CoinConfigs[i].Pool!.CryptocurrencyId;
+                    }
                 }
-
-                if (!device.FlightSheetId.HasValue)
+                catch (Exception ex)
                 {
-                    continue;
-                }
-
-                var flightSheetGettingResult = await _mediator.Send(
-                        new GetFlightSheetByIdQuery(device.FlightSheetId.Value, rigIndicators.UserId));
-
-                if (!TryGetValue(flightSheetGettingResult, out FlightSheetModel flightSheet))
-                {
-                    continue;
-                }
-
-                // todo: перейти на сравнение по типу устройства.
-                var flightSheetTarget = flightSheet.Targets.First(x => x.Miner.Name == deviceIndicators.FlightSheet!.MinerName);
-
-                deviceIndicators.FlightSheet!.Id = flightSheet.Id;
-                deviceIndicators.FlightSheet!.MinerId = flightSheetTarget.Miner.Id;
-
-                for (int i = 0; i < deviceIndicators.FlightSheet.Coins.Count; i++)
-                {
-                    var coinMetrics = deviceIndicators.FlightSheet.Coins[i];
-                    coinMetrics.CoinId = flightSheetTarget.MiningConfig.CoinConfigs[i].Pool!.CryptocurrencyId;
+                    _logger.LogError("Building indicators error - {error}", ex.Message);
                 }
             }
 
