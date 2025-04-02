44d1a7021078bf856070a1c13c9408c53bee1928
Sahnov Alexandr <124876649+SahnovAlex@users.noreply.github.com>
2025-04-02 22:31:24 +0700

    features/set_rig_devices_method: исправил логику сохранения майнинг-устройств из инвенторизации

---
+++ src/Management/MNX.MonitoringCenter.Management.Core/Mining/MiningDevice/Enums/FlightSheetConfirmationState.cs
@@ -0,0 +1,22 @@
+﻿namespace MNX.MonitoringCenter.Management.Core.Mining.MiningDevice.Enums;
+
+/// <summary>
+/// Перечисление состояния подтверждения применения полетного листа.
+/// </summary>
+public enum FlightSheetConfirmationState
+{
+    /// <summary>
+    /// Не подтверждено применение настроек полетного листа.
+    /// </summary>
+    Unconfirmed,
+
+    /// <summary>
+    /// Успешное применение настроек полетного листа.
+    /// </summary>
+    Successfully,
+
+    /// <summary>
+    /// Ошибка применения настроек полетного листа.
+    /// </summary>
+    Error,
+}
---
+++ tests/Inventory/MNX.MonitoringCenter.Inventory.UseCases.IntegrationTests/SetRigDevicesTests.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using MNX.MonitoringCenter.Inventory.IntegrationTests;
+using MNX.MonitoringCenter.Management.UseCases.SetRigDevices;
+using NUnit.Framework;
+
+namespace MNX.MonitoringCenter.Inventory.UseCases.IntegrationTests;
+
+public class SetRigDevicesTests : BaseTest
+{
+    private IMediator _mediator;
+
+    private readonly static Guid OWNER_ID = Guid.Parse("59929758-14d1-43b5-a80c-7e84bfc45145");
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mediator = ServiceProvider.GetRequiredService<IMediator>();
+    }
+
+    [TestCaseSource(typeof(SetRigDevicesTests), nameof(SaveCommandTestCase.InventoryMessages))]
+    public async Task SetRigDevices(SetRigDevicesCommand message)
+    {
+
+    }
+}
