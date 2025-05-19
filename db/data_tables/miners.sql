28e086d538908a2ea16f884c77affbdaaf593d94
Daniil Ibragimov <ghaghal93@gmail.com>
2025-05-19 19:01:54 +0700

    Added new versions for integrity miners

--- db/data_tables/miners.sql
+++ db/data_tables/miners.sql
@@ -1,21 +1,35 @@
-insert into monitoring_center.miners(name, version, mining_mode, supported_devices, type) values
+insert into monitoring_center.miners(name, version, mining_mode, supported_devices, type, installation_url) values
 
-('deroluna-miner', '1.14', 'Single', 4+16, 'Integrated'),
+('deroluna-miner', '1.14', 'Single', 4+16, 'Integrated', 'https://github.com/DeroLuna/dero-miner/releases/download/v1.14/deroluna-v1.14_linux_hiveos_mmpos.tar.gz'),
 
-('xmrig', '6.22.2', 'Single', 4+16, 'Integrated'),
+('xmrig', '6.22.2', 'Single', 4+16, 'Integrated', 'https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz'),
 
-('wildrig-multi', '0.42.2', 'Single', 1+2+8, 'Integrated'),
+('wildrig-multi', '0.43.0', 'Single', 1+2+8, 'Integrated', 'https://github.com/andru-kun/wildrig-multi/releases/download/0.43.0/wildrig-multi-linux-0.43.0.tar.xz'),
+('wildrig-multi', '0.42.9', 'Single', 1+2+8, 'Integrated', 'https://github.com/andru-kun/wildrig-multi/releases/download/0.42.9/wildrig-multi-linux-0.42.9.tar.xz'),
+('wildrig-multi', '0.42.2', 'Single', 1+2+8, 'Integrated', 'https://github.com/andru-kun/wildrig-multi/releases/download/0.42.2/wildrig-multi-linux-0.42.2.tar.xz'),
 
-('t-rex', '0.26.8', 'Dual', 1, 'Integrated'),
+('t-rex', '0.26.8', 'Dual', 1, 'Integrated', 'https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz'),
+('t-rex', '0.26.6', 'Dual', 1, 'Integrated', 'https://github.com/trexminer/T-Rex/releases/download/0.26.6/t-rex-0.26.6-linux.tar.gz'),
+('t-rex', '0.26.5', 'Dual', 1, 'Integrated', 'https://github.com/trexminer/T-Rex/releases/download/0.26.5/t-rex-0.26.5-linux.tar.gz'),
 
-('teamredminer', '0.10.21', 'Triple', 8, 'Integrated'),
+('teamredminer', '0.10.21', 'Triple', 8, 'Integrated', 'https://github.com/todxx/teamredminer/releases/download/v0.10.21/teamredminer-v0.10.21-linux.tgz'),
+('teamredminer', '0.10.20', 'Triple', 8, 'Integrated', 'https://github.com/todxx/teamredminer/releases/download/v0.10.20/teamredminer-v0.10.20-linux.tgz'),
 
-('SRBMiner-MULTI', '2.7.6', 'Triple', 1+2+4+8+16, 'Integrated'),
+('SRBMiner-MULTI', '2.8.8', 'Triple', 1+2+4+8+16, 'Integrated', 'https://github.com/doktor83/SRBMiner-Multi/releases/download/2.8.8/SRBMiner-Multi-2-8-8-Linux.tar.gz'),
+('SRBMiner-MULTI', '2.8.7', 'Triple', 1+2+4+8+16, 'Integrated', 'https://github.com/doktor83/SRBMiner-Multi/releases/download/2.8.7/SRBMiner-Multi-2-8-7-Linux.tar.gz'),
+('SRBMiner-MULTI', '2.7.6', 'Triple', 1+2+4+8+16, 'Integrated', 'https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.6/SRBMiner-Multi-2-7-6-Linux.tar.gz'),
 
-('rigel', '1.20.1', 'Triple', 1, 'Integrated'),
+('rigel', '1.21.3', 'Triple', 1, 'Integrated', 'https://github.com/rigelminer/rigel/releases/download/1.21.3/rigel-1.21.3-linux.tar.gz'),
+('rigel', '1.21.2', 'Triple', 1, 'Integrated', 'https://github.com/rigelminer/rigel/releases/download/1.21.2/rigel-1.21.2-linux.tar.gz'),
+('rigel', '1.20.1', 'Triple', 1, 'Integrated', 'https://github.com/rigelminer/rigel/releases/download/1.20.1/rigel-1.20.1-linux.tar.gz'),
 
-('onezerominer', '1.4.3', 'Dual', 1, 'Integrated'),
+('onezerominer', '1.4.4', 'Dual', 1, 'Integrated', 'https://github.com/OneZeroMiner/onezerominer/releases/download/v1.4.4/onezerominer-linux-1.4.4.tar.gz'),
+('onezerominer', '1.4.3', 'Dual', 1, 'Integrated', 'https://github.com/OneZeroMiner/onezerominer/releases/download/v1.4.3/onezerominer-linux-1.4.3.tar.gz'),
 
-('lolMiner', '1.94a', 'Dual', 1+2+8, 'Integrated'),
+('lolMiner', '1.95', 'Dual', 1+2+8, 'Integrated', 'https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.95/lolMiner_v1.95_Lin64.tar.gz'),
+('lolMiner', '1.95a', 'Dual', 1+2+8, 'Integrated', 'https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.95a/lolMiner_v1.95a_Lin64.tar.gz'),
+('lolMiner', '1.94a', 'Dual', 1+2+8, 'Integrated', 'https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.94a/lolMiner_v1.94a_Lin64.tar.gz'),
 
-('gminer', '3.44', 'Single', 1+8, 'Integrated');
+('gminer', '3.44', 'Single', 1+8, 'Integrated', 'https://github.com/develsoftware/GMinerRelease/releases/download/3.44/gminer_3_44_linux64.tar.xz'),
+('gminer', '3.43', 'Single', 1+8, 'Integrated', 'https://github.com/develsoftware/GMinerRelease/releases/download/3.43/gminer_3_43_linux64.tar.xz'),
+('gminer', '3.42', 'Single', 1+8, 'Integrated', 'https://github.com/develsoftware/GMinerRelease/releases/download/3.42/gminer_3_42_linux64.tar.xz');
