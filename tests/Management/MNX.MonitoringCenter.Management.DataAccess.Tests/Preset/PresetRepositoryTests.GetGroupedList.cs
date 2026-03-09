903cc651c9bf4ab0f6a631bfcb1ca3e4d953b497
Sahnov Alexandr <124876649+SahnovAlex@users.noreply.github.com>
2026-03-09 19:45:30 +0700

    - реализованы тестировочные методы для методов GetAllAvailable и GetGroupedList класса PresetRepository; - исправлена бага, при которой значение IsVisible из базы данных не присваивалось получаемой сущности;

---
+++ tests/Management/MNX.MonitoringCenter.Management.DataAccess.Tests/Preset/PresetRepositoryTests.GetGroupedList.cs
@@ -0,0 +1,41 @@
+﻿using MNX.MonitoringCenter.Management.UseCases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNX.MonitoringCenter.Management.DataAccess.Tests.Preset;
+
+using Preset = Core.Overclocking.Preset;
+
+public partial class PresetRepositoryTests
+{
+    [TestCaseSource(typeof(PresetsTestCaseSource), nameof(PresetsTestCaseSource.PresetsSeparatedByDeviceNameGroups))]
+    public async Task GetGroupedList_ValidUserId_ReturnsListOfGroups(List<Preset> data)
+    {
+        // Arrange
+
+        var userId = PresetsTestCaseSource.UserId;
+        var specification = new Specification(userId);
+        var query = data.GroupBy(x => x.DeviceName);
+        var expectedCount = query.Count();
+
+        await PrepareDataBase(data);
+
+
+        // Act
+
+        var checkingPresetGroups = await _presetRepository
+            .GetGroupedList(x => x.DeviceName, specification).ToListAsync();
+
+
+        // Assert
+
+        Assert.That(checkingPresetGroups, Is.Not.Null);
+        Assert.That(checkingPresetGroups, Has.Count.EqualTo(expectedCount));
+
+        var checkingPresets = checkingPresetGroups.SelectMany(x => x).ToList();
+        AssertPresetsWithNvidiaGpuOverclocking(data, checkingPresets);
+    }
+}
---
+++ tests/Management/MNX.MonitoringCenter.Management.DataAccess.Tests/Preset/PresetRepositoryTests.cs
@@ -0,0 +1,134 @@
+﻿using AutoMapper;
+using MNX.MonitoringCenter.Management.DataAccess.Mapping;
+using MNX.MonitoringCenter.Management.DataAccess.Preset;
+using MNX.MonitoringCenter.Management.DataAccess.Tests.Infrastructure;
+using MNX.MonitoringCenter.Management.Tests.Service.Builders.CoreBuilders;
+using MNX.MonitoringCenter.Management.Tests.Service.Builders.CoreBuilders.Overclockings;
+using MNX.MonitoringCenter.Management.UseCases.Overclocking.Presets;
+
+namespace MNX.MonitoringCenter.Management.DataAccess.Tests.Preset;
+
+using Preset = Core.Overclocking.Preset;
+
+public partial class PresetRepositoryTests : BaseTest
+{
+    private IPresetRepository _presetRepository;
+    private IMapper _mapper;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new DbMappingProfile());
+        });
+        _mapper = config.CreateMapper();
+
+        _presetRepository = new PresetRepository(Context, _mapper);
+    }
+
+    private static class PresetsTestCaseSource
+    {
+        public static Guid UserId = Guid.NewGuid();
+        public static string NvidiaGpuName = "Nvidia Geforce RTX 4080 ti";
+
+        public static IEnumerable<List<Preset>> PresetsWithNvidiaGpuOverclocking
+        {
+            get
+            {
+                yield return
+                [
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithDeviceName(NvidiaGpuName)
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithDeviceName(NvidiaGpuName)
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithDeviceName(NvidiaGpuName)
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                ];
+            }
+        }
+
+        public static IEnumerable<List<Preset>> PresetsSeparatedByDeviceNameGroups
+        {
+            get
+            {
+                yield return
+                [
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithDeviceName("Nvidia Geforce RTX 4080 ti")
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithDeviceName("Nvidia Geforce RTX 4080 ti")
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithDeviceName("Nvidia Geforce RTX 4070")
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithDeviceName("Nvidia Geforce RTX 4070")
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithDeviceName("Nvidia GeForce RTX 5080")
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                    new PresetBuilder()
+                        .WithOwnerId(UserId)
+                        .WithVisible()
+                        .WithDeviceName("Nvidia GeForce RTX 5080")
+                        .WithOverclocking(() => new NvidiaGpuOverclockingBuilder().Build())
+                        .Build(),
+                ];
+            }
+        }
+    }
+}
